85b93f143bb30a8f0c5426f51d76b1f261417045
lipengyu2019 <lpy_cf@126.com>
2020-04-14 18:56:59 +0800

    1

--- demo/src/http.js
+++ demo/src/http.js
@@ -5,7 +5,7 @@ asxios.defaults.baseURL = '/api'
 const http = asxios.create({
     // baseURL: process.env.VUE_APP_API_URL || '/web/api',
     //baseURL: 'http://59.196.99.4/gateway/api/1/get_qiye?ENTNAME=加贝',
-    baseURL: 'http://127.0.0.1/',
+    baseURL: 'http://127.0.0.1:8080/',
     //headers: { 'AppKey': '688337637353717760' },
     //withCredentials: false,
 })
