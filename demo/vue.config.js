88e48f1b6ba576a078d17487b935e3be265cdd7a
lipengyu2019 <lpy_cf@126.com>
2020-04-14 13:28:46 +0800

    2

--- demo/vue.config.js
+++ demo/vue.config.js
@@ -5,12 +5,12 @@ module.exports = {
     devServer: {
         open: true, //是否自动弹出浏览器页面
         host: "localhost",
-        port: '8080',
+        port: '8081',
         https: false,   //是否使用https协议
         hotOnly: false, //是否开启热更新
         proxy: {
             '/api': {
-                target: 'http://59.196.99.4/gateway/api/1/get_qiye?ENTNAME=加贝', //API服务器的地址
+                target: 'http://59.196.99.4/gateway', //API服务器的地址
                 changeOrigin: true,
                 pathRewrite: {
                     '^/api': ''
