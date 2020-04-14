645ba2732b9d4b128889a163f7a5edd1fcd60265
lipengyu2019 <lpy_cf@126.com>
2020-04-14 11:35:58 +0800

    1

--- demo/src/http.js
+++ demo/src/http.js
@@ -1,9 +1,10 @@
 import asxios from "axios";
-import Vue from 'vue'
-import router from "./router/index";
+// import Vue from 'vue'
+// import router from "./router/index";
 const http = asxios.create({
     // baseURL: process.env.VUE_APP_API_URL || '/web/api',
-     baseURL: 'http://127.0.0.1:3000/web/api/'
+    baseURL: 'http://59.196.99.4/gateway/api/1/get_qiye?ENTNAME=加贝',
+    headers: { 'AppKey': '688337637353717760' },
 })
 
 export default http
